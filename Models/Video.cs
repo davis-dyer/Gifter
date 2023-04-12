03d19a786df822af0d52317840390a6dc161ad22
Davis Dyer <davis.dyer10@gmail.com>
2023-04-12 14:19:10 -0400

    v 1.0

---
+++ Models/Video.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Gifter.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public string Caption { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+    }
+}
---
+++ Repositories/IVideoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IVideoRepository
+    {
+        List<Video> GetAll();
+        void Add(Video video);
+        void Update(Video video);
+        void Delete(int id);
+        public Video GetById(int id);
+        List<Video> GetAllWithComments();
+
+    }
+}
